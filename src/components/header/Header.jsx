e2577213192dd78fdb2ba3e37d366b4169b33ad1
dagi3244 <tttfffqr@gmail.com>
2024-01-19 00:22:54 -0800

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -99,7 +99,7 @@ function Header() {
                       <img
                         src={
                           user?.imageBlob
-                            ? `https://evangadi-forum-backend-k8wj.onrender.com/api/all/images/${
+                            ? `https://evangadi-forum-backend-wqf7.onrender.com/api/all/images/${
                                 user.imageBlob[user?.userid]
                               }`
                             : ""
