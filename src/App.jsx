7bda8e4a6278c2eea702ceb7247c98d3162394d4
dagi3244 <tttfffqr@gmail.com>
2024-01-20 14:05:21 -0800

    m

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import Comment from "./components/comment/Comment";
 import AuthRequired from "./components/Authentication/AuthRequired";
-axios.defaults.baseURL = "https://evangadi-forum-backend-k8wj.onrender.com";
+axios.defaults.baseURL = "https://evangadi-forum-backend-otkh.onrender.com";
 export const AuthContext = createContext();
 function App() {
   const [user, setUser] = useState({});
